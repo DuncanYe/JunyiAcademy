7d1e3d6279d55c668ea778bc6836efd096fb1b79
Duncan <loxa10077@gmail.com>
2020-03-16 11:27:38 +0800

    create filter_integer method

--- run.rb
+++ run.rb
@@ -2,9 +2,7 @@ def reverse_string(str)
   new_str = str.reverse
   puts new_str
 end
-
-
-reverse_string("abc")
+# reverse_string("abc")
 
 
 def reverse_every_character_string(str)
@@ -13,6 +11,24 @@ def reverse_every_character_string(str)
   end
   puts new_str.join(" ")
 end
+# reverse_every_character_string("abc qwe ert")
 
 
-reverse_every_character_string("abc qwe ert")
+def filter_integer(n)
+  data = [*1..n]
+  final_number= []
+
+  data.each do |i|
+    if i % 3 == 0 && i % 5 == 0
+      final_number << i
+    elsif i % 3 == 0 || i % 5 == 0
+      # delete
+    else
+      final_number << i
+    end
+  end
+  puts final_number
+  
+end
+
+filter_integer(30)
