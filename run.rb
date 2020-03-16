9193fb5037b8702ae347c98402cfdafffeda1830
Duncan <loxa10077@gmail.com>
2020-03-16 11:10:29 +0800

    create reverse_every_character_string method

--- run.rb
+++ run.rb
@@ -5,3 +5,14 @@ end
 
 
 reverse_string("abc")
+
+
+def reverse_every_character_string(str)
+  new_str = str.split.map do |s|
+    s.reverse
+  end
+  puts new_str.join(" ")
+end
+
+
+reverse_every_character_string("abc qwe ert")
